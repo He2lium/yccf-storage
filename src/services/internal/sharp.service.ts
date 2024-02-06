cfec4f9cb0d97c173d0ec6fcf77f612d54e78a5a
pavelprna <pavel@prna.ru>
2024-02-06 10:42:45 +0700

    fix: fit

--- src/services/internal/sharp.service.ts
+++ src/services/internal/sharp.service.ts
@@ -1,16 +1,18 @@
-import {YcfStorageImageOptionsType} from "../../types/ycf-storage-image-options.type";
+import { YcfStorageImageOptionsType } from "../../types/ycf-storage-image-options.type";
 import sharp from "sharp";
 
+export const SharpService = async (
+  buffer: Buffer,
+  options: YcfStorageImageOptionsType,
+) => {
+  const source = sharp(buffer);
+  if (options.resize) {
+    const { width, height, fit } = options.resize;
+    if (width && height) source.resize(width, height, { fit });
+    else if (width) source.resize(width, null, { fit });
+    else if (height) source.resize(null, height, { fit });
+  }
 
-export const SharpService = async (buffer: Buffer, options: YcfStorageImageOptionsType) => {
-    const source = sharp(buffer)
-    if (options.resize) {
-        const {width, height, fit} = options.resize
-        if (width && height) source.resize(width, height)
-        else if (width) source.resize(width, null, {fit})
-        else if (height) source.resize(null, height, {fit})
-    }
-
-    source.toFormat(options.to,options.toOptions)
-    return source.toBuffer()
-}
+  source.toFormat(options.to, options.toOptions);
+  return source.toBuffer();
+};
