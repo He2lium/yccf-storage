8f203f0364a4b237d2d6de41b550f1d2c728c893
pavelprna <pavel@prna.ru>
2024-02-22 08:30:38 +0700

    refactor: add YcfStorageMqType generic

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,16 @@
-import {YcfStorageActionHandlerService} from "./services/ycf-storage-action-handler.service";
-import {YcfStorageCloudFunctionHandlerService} from "./services/ycf-storage-cloud-function-handler.service";
-import {YcfStorageCallbackType} from "./types/ycf-storage-callback.type";
-import {YcfStorageImageOptionsType} from "./types/ycf-storage-image-options.type";
-import {YcfStorageActionEnum} from "./types/ycf-storage-action.enum";
-import {
-    YcfStorageActionDeleteType,
-    YcfStorageActionMoveType,
-    YcfStorageActionMoveImageWithHandlingType,
-    YcfStorageActionDownloadImageWithHandlingType,
-    YcfStorageActionDownloadType,
-    YcfStorageActionImageType
-} from "./types/ycf-storage-actions.type";
-import {YcfStorageAnyActionType} from "./types/ycf-storage-any-action.type";
-import {YcfStorageMqType} from "./types/ycf-storage-mq.type";
-import {YcfStorageS3optionsType} from "./types/ycf-storage-s3options.type";
-
+export { YcfStorageActionHandlerService } from "./services/ycf-storage-action-handler.service";
+export { YcfStorageCloudFunctionHandlerService } from "./services/ycf-storage-cloud-function-handler.service";
+export { YcfStorageCallbackType } from "./types/ycf-storage-callback.type";
+export { YcfStorageImageOptionsType } from "./types/ycf-storage-image-options.type";
+export { YcfStorageActionEnum } from "./types/ycf-storage-action.enum";
+export { YcfStorageAnyActionType } from "./types/ycf-storage-any-action.type";
+export { YcfStorageMqType } from "./types/ycf-storage-mq.type";
+export { YcfStorageS3optionsType } from "./types/ycf-storage-s3options.type";
 export {
-    YcfStorageActionHandlerService,
-    YcfStorageCloudFunctionHandlerService,
-
-    YcfStorageCallbackType,
-    YcfStorageImageOptionsType,
-    YcfStorageActionEnum,
-    YcfStorageAnyActionType,
-    YcfStorageMqType,
-    YcfStorageS3optionsType,
-
-    // Actions
-    YcfStorageActionDeleteType,
-    YcfStorageActionMoveType,
-    YcfStorageActionMoveImageWithHandlingType,
-    YcfStorageActionDownloadImageWithHandlingType,
-    YcfStorageActionDownloadType,
-    YcfStorageActionImageType
-}
+  YcfStorageActionDeleteType,
+  YcfStorageActionMoveType,
+  YcfStorageActionMoveImageWithHandlingType,
+  YcfStorageActionDownloadImageWithHandlingType,
+  YcfStorageActionDownloadType,
+  YcfStorageActionImageType,
+} from "./types/ycf-storage-actions.type";
