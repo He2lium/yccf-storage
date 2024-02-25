3f7c912bcdac837bd8e590e3e0e9679ecff7d3a6
pavelprna <pavel@prna.ru>
2024-02-25 08:28:47 +0700

    refactor: delete objects command

--- src/services/internal/s3/delete.s3-service.ts
+++ src/services/internal/s3/delete.s3-service.ts
@@ -1,4 +1,4 @@
-import { DeleteObjectCommand, DeleteObjectsCommand } from "@aws-sdk/client-s3";
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { globalRefS3Service } from "../global-ref-s3.service";
 
 export const DeleteS3Service = async (Keys: string[]) => {
